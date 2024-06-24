0345456c4165a85e645e79f2bf85bf6e5df7add7
Azeem Kamal <azeem.kamal@innotix.com>
2024-06-24 17:03:11 +0200

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import logo from '../images/logo.svg';
+import {pageLinks, socialLinks} from "../data";
+import SocialLinks from "./SocialLinks";
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <img src={logo} className="nav-logo" alt="backroads"/>
+                    <button type="button" className="nav-toggle" id="nav-toggle">
+                        <i className="fas fa-bars"></i>
+                    </button>
+                </div>
+                <ul className="nav-links" id="nav-links">
+                    {pageLinks.map((link)=> {
+                        const {id, href, text} = link
+                        return (
+                            <li key={id}>
+                            <a href={href} className="nav-link">{text}</a>
+                        </li>)
+                    })}
+                </ul>
+
+                <ul className="nav-icons">
+                    {socialLinks.map((link) => {
+                        return (
+                            <SocialLinks key={link.id} {...link} itemClass='nav-icon'/>
+                        )
+                    })}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
